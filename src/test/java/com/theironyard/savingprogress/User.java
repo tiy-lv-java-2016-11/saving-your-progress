453d113482a1cf093edfc39608de072069c8c35b
GreedOrDie <greedordie@gmail.com>
2016-12-08 08:11:13 -0800

    Here is my current  saving progress assignment. There will be a second commit coming sometime today.

---
+++ src/test/java/com/theironyard/savingprogress/User.java
@@ -0,0 +1,26 @@
+package com.theironyard.savingprogress;
+
+import java.util.Scanner;
+
+/**
+ * Created by darionmoore on 12/7/16.
+ */
+public class User {
+    private String name;
+
+
+    public User(){}
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
